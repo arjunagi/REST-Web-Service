80a6231b4091ba58ac197d159a795520f43e709f
arjunagi <karthik.arjunagi@gmail.com>
2017-05-29 03:53:15 -0700

    added the first 7 GET rules

--- src/main/java/RequestProjectService.java
+++ src/main/java/RequestProjectService.java
@@ -2,10 +2,9 @@ import com.google.gson.Gson;
 import spark.Request;
 import java.io.BufferedReader;;
 import java.io.FileReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by Karthik on 5/28/17.
@@ -21,9 +20,11 @@ public class RequestProjectService {
         String projectId = request.queryParams("projectid");
 
         Map<String, String[]> parameterMap = request.queryMap().toMap();
+        if(parameterMap.isEmpty())
+            return getHighestCostProject();
         if(!validateParameterMap(parameterMap))
             return new ResponseMessageWithStatusCode("Invalid parameter in the request", 400);
-        return getProject(projectId);
+        return getProject(projectId, parameterMap);
     }
 
     /**
@@ -31,7 +32,7 @@ public class RequestProjectService {
      * @param projectId
      * @return
      */
-    private Response getProject(String projectId) {
+    private Response getProject(String projectId, Map<String, String[]> parameterMap) {
 
         try(BufferedReader br = new BufferedReader(new FileReader("projects.txt"))) {
             for(String line; (line = br.readLine()) != null; ) {
@@ -67,4 +68,70 @@ public class RequestProjectService {
         }
         return true;
     }
+
+    /**
+     *
+     * @param parameterMap
+     * @param project
+     * @return
+     */
+    private boolean passesAllRules(Map<String,String[]> parameterMap, Project project) {
+
+        return true;
+    }
+
+    /**
+     *
+     * @return
+     */
+    private Response getHighestCostProject() {
+
+        Project highestCostProject = null;
+        double highestCost = Double.MIN_VALUE;
+
+        try(BufferedReader br = new BufferedReader(new FileReader("projects.txt"))) {
+
+            for(String line; (line = br.readLine()) != null; ) {
+
+                Project project = new Gson().fromJson(line, Project.class);
+
+                if(!project.isValid() || project.getEnabled().equals(false) || isExpired(project.getExpiryDate()) || project.getProjectUrl().equals(null))
+                    continue;
+                else if(project.getProjectCost() > highestCost) {
+                    highestCost = project.getProjectCost();
+                    highestCostProject = project;
+                }
+            }
+
+            if(highestCost == Double.MIN_VALUE )
+                return new ResponseMessage("no project found");
+            else {
+                return new ResponseProject(highestCostProject.getProjectName(), highestCostProject.getProjectCost(), highestCostProject.getProjectUrl());
+            }
+
+        } catch (Exception e) {
+            return new ResponseMessageWithStatusCode(e + ": Error during file read", 500);
+        }
+    }
+
+    /**
+     *
+     * @param ed
+     * @return
+     */
+    private boolean isExpired(String ed) {
+        DateFormat df = new SimpleDateFormat("MMddyyyy HH:mm:ss");
+        try {
+            Date expiryDate = df.parse(ed);
+            Date currentDate = new Date();
+            if(currentDate.after(expiryDate)) {
+                return true;
+            }
+        } catch (Exception e) {
+            return true;
+        }
+        return false;
+    }
+
+
 }
